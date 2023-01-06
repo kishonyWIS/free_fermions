6f5cdd1d8ac3ae8afe8ea2e941e63e6cd213a2c0
giladkishony <giladkishony@gmail.com>
2023-01-06 21:48:43 +0200

    Add option for HamiltonianTerm on single pair of fermions. Add tensor to SinglePartibleDensityMatrix

--- free_fermion_hamiltonian.py
+++ free_fermion_hamiltonian.py
@@ -1,4 +1,4 @@
-from typing import List, Union, Tuple, Callable, Optional
+from typing import Union, Callable, Optional
 import numpy as np
 from scipy.integrate import complex_ode
 from scipy.linalg import eigh, eig
@@ -10,17 +10,17 @@ from scipy.stats import special_ortho_group
 # TODO: guage field
 
 
-def add_to_diag(arr: np.ndarray, to_add: Union[int, List]):
+def add_to_diag(arr: np.ndarray, to_add: Union[int, list]):
     idx = np.diag_indices_from(arr)
     arr[idx] += to_add
 
 
-def tensor_to_matrix(tensor: np.ndarray, system_shape: Tuple[int, ...]) -> np.ndarray:
+def tensor_to_matrix(tensor: np.ndarray, system_shape: tuple[int, ...]) -> np.ndarray:
     matrix_shape = get_system_matrix_shape(system_shape)
     return tensor.reshape(matrix_shape[0], matrix_shape[1])
 
 
-def matrix_to_tensor(matrix: np.ndarray, system_shape: Tuple[int, ...]) -> np.ndarray:
+def matrix_to_tensor(matrix: np.ndarray, system_shape: tuple[int, ...]) -> np.ndarray:
     return matrix.reshape(system_shape)
 
 
@@ -37,16 +37,20 @@ def site_and_sublattice_to_flat_index(site, sublattice, system_shape):
 
 class HamiltonianTerm:
     def __init__(self,
-                 strength: Union[float, List[float]],
+                 strength: Union[float, list[float]],
                  sublattice1: int,
                  sublattice2: int,
-                 site_offset: Union[int, Tuple[int]],
-                 system_shape: Tuple[int, ...],
+                 system_shape: tuple[int, ...],
+                 site1: Optional[int] = None,
+                 site2: Optional[int] = None,
+                 site_offset: Union[int, tuple[int]] = None,
                  time_dependence: Optional[Callable] = None):
         self.system_shape = system_shape
         self.site_offset = site_offset
         self.sublattice1 = sublattice1
         self.sublattice2 = sublattice2
+        self.site1 = site1
+        self.site2 = site2
         self.strength = strength
         self.time_dependence = time_dependence
 
@@ -58,12 +62,18 @@ class HamiltonianTerm:
     def strength(self, new_strength):
         self._strength = new_strength
         self._time_independent_tensor = np.zeros(self.system_shape)
-        add_to_diag(
-            self._time_independent_tensor[:self.system_shape[0] - self.site_offset, self.sublattice1, self.site_offset:,
-            self.sublattice2], 2 * self._strength)
-        add_to_diag(
-            self._time_independent_tensor[self.site_offset:, self.sublattice2, :self.system_shape[2] - self.site_offset,
-            self.sublattice1], -2 * self._strength)
+        if self.site1 is not None and self.site2 is not None:
+            self._time_independent_tensor[self.site1, self.sublattice1, self.site2, self.sublattice2] += \
+                2*self._strength
+            self._time_independent_tensor[self.site2, self.sublattice2, self.site1, self.sublattice1] -= \
+                2*self._strength
+        elif self.site_offset is not None:
+            add_to_diag(
+                self._time_independent_tensor[:self.system_shape[0] - self.site_offset, self.sublattice1, self.site_offset:,
+                self.sublattice2], 2 * self._strength)
+            add_to_diag(
+                self._time_independent_tensor[self.site_offset:, self.sublattice2, :self.system_shape[2] - self.site_offset,
+                self.sublattice1], -2 * self._strength)
         self._time_independent_matrix = tensor_to_matrix(self._time_independent_tensor, self.system_shape)
 
     @property
@@ -90,16 +100,18 @@ class HamiltonianTerm:
 
 
 class FreeFermionHamiltonian:
-    def __init__(self, system_shape: Tuple[int, ...]):
+    def __init__(self, system_shape: tuple[int, ...]):
         self.terms = {}
         self.system_shape = system_shape
 
     def add_term(self,
                  name: str,
-                 strength: Union[float, List[float]],
+                 strength: Union[float, list[float]],
                  sublattice1: int,
                  sublattice2: int,
-                 site_offset: Union[int, Tuple[int, ...]],
+                 site1: Optional[int] = None,
+                 site2: Optional[int] = None,
+                 site_offset: Union[int, tuple[int, ...]] = None,
                  time_dependence: Optional[Callable] = None):
         """Adds a term sum_on_j{i*strength_j*c_j^sublattice1*c_j+site_offset^sublattice2}
         This term is symmetricized as
@@ -109,8 +121,9 @@ class FreeFermionHamiltonian:
         M_j^sublattice1_j+site_offset^sublattice2 += 2*strength_j
         M_j+site_offset^sublattice2_j^sublattice1 += -2*strength_j
         """
-        self.terms[name] = HamiltonianTerm(strength, sublattice1, sublattice2, site_offset, self.system_shape,
-                                           time_dependence)
+        self.terms[name] = HamiltonianTerm(strength=strength, sublattice1=sublattice1, sublattice2=sublattice2,
+                                           site1=site1, site2=site2, site_offset=site_offset,
+                                           system_shape=self.system_shape, time_dependence=time_dependence)
 
     def get_tensor(self, t=None):
         return sum([x.get_time_dependent_tensor(t) for x in self.terms.values()])
@@ -141,13 +154,29 @@ class FreeFermionHamiltonian:
 
 
 class SingleParticleDensityMatrix:
-    def __init__(self, system_shape: Tuple[int, ...], matrix: np.ndarray = None):
+    def __init__(self, system_shape: tuple[int, ...], matrix: np.ndarray = None, tensor: np.ndarray = None):
         self.system_shape = system_shape
-        self._matrix = matrix
+        self.matrix = None
+        if matrix is not None:
+            self.matrix = matrix
+        if tensor is not None:
+            self.tensor = tensor
 
     @property
     def matrix(self):
         return self._matrix
+
+    @matrix.setter
+    def matrix(self, new_matrix):
+        self._matrix = new_matrix
+
+    @property
+    def tensor(self):
+        return matrix_to_tensor(self.matrix, self.system_shape)
+
+    @tensor.setter
+    def tensor(self, new_tensor):
+        self._matrix = tensor_to_matrix(new_tensor, self.system_shape)
 
     def randomize(self):
         self._matrix = np.zeros(get_system_matrix_shape(self.system_shape))
@@ -181,8 +210,12 @@ def get_B(t):
 
 
 num_sites = 10
-num_sublattices = 4
+num_sublattices = 6
 system_shape = (num_sites, num_sublattices, num_sites, num_sublattices)
+non_gauge_shape = (num_sites, 4, num_sites, 4)
+gauge_shape = (num_sites, 2, num_sites, 2)
+non_gauge_idxs = np.ix_(range(num_sites),[0,3,4,5],range(num_sites),[0,3,4,5])
+gauge_idxs = np.ix_(range(num_sites),[1,2],range(num_sites),[1,2])
 h = 1
 J = 0.7
 g0 = 0.25
@@ -193,29 +226,38 @@ t1 = T / 4
 u = np.ones(num_sites - 1)
 
 hamiltonian = FreeFermionHamiltonian(system_shape)
-hamiltonian.add_term(name='h', strength=h, sublattice1=1, sublattice2=0, site_offset=0, time_dependence=None)
-hamiltonian.add_term(name='J', strength=-J * u, sublattice1=1, sublattice2=0, site_offset=1, time_dependence=None)
-hamiltonian.add_term(name='g', strength=-1, sublattice1=2, sublattice2=0, site_offset=0, time_dependence=get_g)
-hamiltonian.add_term(name='B', strength=-1, sublattice1=2, sublattice2=3, site_offset=0, time_dependence=get_B)
+hamiltonian.add_term(name='h', strength=h, sublattice1=3, sublattice2=0, site_offset=0, time_dependence=None)
+hamiltonian.add_term(name='J', strength=-J * u, sublattice1=3, sublattice2=0, site_offset=1, time_dependence=None)
+hamiltonian.add_term(name='g', strength=-1, sublattice1=4, sublattice2=0, site_offset=0, time_dependence=get_g)
+hamiltonian.add_term(name='B', strength=-1, sublattice1=4, sublattice2=5, site_offset=0, time_dependence=get_B)
 
 decoupled_hamiltonian = FreeFermionHamiltonian(system_shape)
-decoupled_hamiltonian.add_term(name='h', strength=h, sublattice1=1, sublattice2=0, site_offset=0, time_dependence=None)
-decoupled_hamiltonian.add_term(name='J', strength=-J * u, sublattice1=1, sublattice2=0, site_offset=1, time_dependence=None)
+decoupled_hamiltonian.add_term(name='h', strength=h, sublattice1=3, sublattice2=0, site_offset=0, time_dependence=None)
+decoupled_hamiltonian.add_term(name='J', strength=-J * u, sublattice1=3, sublattice2=0, site_offset=1, time_dependence=None)
 decoupled_hamiltonian_matrix = decoupled_hamiltonian.get_matrix()
 ground_state = decoupled_hamiltonian.get_ground_state()
+
+gauge_setting_hamiltonian = FreeFermionHamiltonian(system_shape)
+gauge_setting_hamiltonian.add_term(name='G', strength=-1, sublattice1=2, sublattice2=1, site_offset=1)
+S_gauge = gauge_setting_hamiltonian.get_ground_state()
 
 integration_params = dict(name='vode', nsteps=2000, rtol=1e-6, atol=1e-12)
 
 
 Ud = hamiltonian.full_cycle_unitary(integration_params, 0, T)
 
-S = SingleParticleDensityMatrix(system_shape)
-S.randomize()
+S_non_gauge = SingleParticleDensityMatrix(non_gauge_shape)
+S_non_gauge.randomize()
+S0_tensor = np.zeros(system_shape, dtype=complex)
+S0_tensor[non_gauge_idxs] = S_non_gauge.tensor
+S0_tensor[gauge_idxs] = S_gauge.tensor[gauge_idxs]
+S = SingleParticleDensityMatrix(system_shape=system_shape, tensor=S0_tensor)
+
 for _ in range(50):
     print(S.get_energy(decoupled_hamiltonian_matrix))
     # plt.imshow(np.real(S.matrix))
     # plt.show()
     S.evolve_with_unitary(Ud)
     for i in range(num_sites):
-        S.reset(2,3,i,i)
+        S.reset(4,5,i,i)
 print(ground_state.get_energy(decoupled_hamiltonian_matrix))
