edd734b62e8cf921bf28639f7761bd1c709e613b
giladkishony <giladkishony@gmail.com>
2023-03-15 18:38:14 +0200

    add energy_density_vs_h_draw_graph, system_spectrum_draw_graph.py

--- system_spectrum_draw_graph.py
+++ system_spectrum_draw_graph.py
@@ -6,34 +6,32 @@ import matplotlib as mpl
 import pandas as pd
 import numpy as np
 
-trotter_steps = 100
-cycles = 1000
-J = 1.0
-V = 0.0
+Ns = 16
+periodic_bc = False
 
-results_df = pd.read_csv("/Users/giladkishony/Dropbox/GILAD/Keva/phd/quantum computation/Periodic Unitaries and Measurements/TestCode/results_energy_density_vs_error_rate.csv")
+results_df = pd.read_csv("/Users/giladkishony/Dropbox/GILAD/Keva/phd/quantum computation/Periodic Unitaries and Measurements/TestCode/results_system_spectrum.csv")
 
-results_df = results_df.query(f"Nt == {trotter_steps} & N_iter == {cycles} & V == {V} & J == {J} & Ns <= 6 & h > 0")
-
+results_df = results_df.query(f"Ns == {Ns} & periodic_bc == {periodic_bc}")
+results_df['h_minus_J'] = results_df.h-results_df.J
+results_df = results_df[['h_minus_J', 'energies', 'V']]
 
 markers = itertools.cycle(['o', 's', '^', '*', '8', 'p', 'd', 'v'])
+colors = itertools.cycle(['b', 'r'])
 
 with sns.axes_style("whitegrid"):
     plt.rcParams["font.family"] = "Times New Roman"
-    fig, ax = plt.subplots()
-    groups = results_df.groupby(["Ns"])
-    for Ns, group in groups:
+
+    groups_V = results_df.groupby(["V"])
+    for V, group_V in groups_V:
+        fig, ax = plt.subplots()
         marker = next(markers)
-        # plt.plot(group.errors_per_cycle_per_qubit, group.energy_density, linestyle='None', marker=marker, label=f'J = {J}, h = {h}')
-        yerr = np.array(group.energy_density_std)
-        yerr[yerr>group.energy_density] = 0.9999*group.energy_density[yerr>group.energy_density]
-        plt.errorbar(group.h, group.energy_density, yerr=yerr, linestyle='--', marker=marker, label=f'N = {Ns}')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    plt.xlabel('h', fontsize='20', fontname='Times New Roman')#, fontweight='bold')
-    plt.ylabel('Energy density', fontsize='20', fontname='Times New Roman')#, fontweight='bold')
-    plt.tick_params(axis='both', which='major', labelsize=15)
-    plt.legend(prop=mpl.font_manager.FontProperties(family='Times New Roman', size=15))
-    plt.tight_layout()
-    plt.savefig(f'graphs/energy_vs_h_steps_{trotter_steps}_cycles_{cycles}_J_{J}_V_{V}.pdf')
+        color = next(colors)
+        energies_groups = group_V.groupby('h_minus_J').energies.apply(np.array)
+        ax.plot(energies_groups.index, np.stack(energies_groups.values), linestyle='--', marker=marker, color=color)
+        plt.xlabel('h-J', fontsize='20', fontname='Times New Roman')
+        plt.ylabel('Energy', fontsize='20', fontname='Times New Roman')
+        ax.tick_params(axis='both', which='major', labelsize=15)
+        # ax.legend(prop=mpl.font_manager.FontProperties(family='Times New Roman', size=15))
+        plt.tight_layout()
+        plt.savefig(f'graphs/system_spectrum_periodic_bc_{periodic_bc}_Ns_{Ns}_V_{V}.pdf')
     plt.show()
