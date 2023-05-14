e8d91ece0bedea87698959194fd93487c2fc4594
giladkishony <giladkishony@gmail.com>
2023-05-14 21:50:56 +0300

    add vortex

--- FloquetKSL.py
+++ FloquetKSL.py
@@ -1,12 +1,6 @@
-from time import time
-from typing import Callable
-import cmocean
-
 import numpy as np
-import scipy
-
-from free_fermion_hamiltonian import MajoranaFreeFermionHamiltonian, MajoranaSingleParticleDensityMatrix, get_fermion_bilinear_unitary
-from scipy.linalg import eigh, eig
+from free_fermion_hamiltonian import MajoranaFreeFermionHamiltonian
+from scipy.linalg import eig
 from matplotlib import pyplot as plt
 
 
@@ -15,7 +9,7 @@ def get_J(t):
     return int(t - np.floor(t) < 1/3)
 
 
-def get_floquet_KSL_model(num_sites_x, num_sites_y, J):
+def get_floquet_KSL_model(num_sites_x, num_sites_y, J, vortex_location=None):
     num_sublattices = 2
     lattice_shape = (num_sites_x, num_sites_y, num_sublattices)
     system_shape = lattice_shape*2
@@ -25,18 +19,44 @@ def get_floquet_KSL_model(num_sites_x, num_sites_y, J):
     site_offset_x = (0, 0)
     site_offset_y = (1, 0)
     site_offset_z = (0, 1)
-    add_J_term(J, 'x', hamiltonian, site_offset_x, 0, 1, lattice_shape, 0)
-    add_J_term(J, 'y', hamiltonian, site_offset_y, 1, 0, lattice_shape, 1/3)
-    add_J_term(J, 'z', hamiltonian, site_offset_z, 1, 0, lattice_shape, 2/3)
+    J_x_strength = np.ones(lattice_shape[:-1] - np.array(site_offset_x)) * J
+    J_y_strength = np.ones(lattice_shape[:-1] - np.array(site_offset_y)) * J
+    J_z_strength = np.ones(lattice_shape[:-1] - np.array(site_offset_z)) * J
+    # J_x_strength[2,2] = -J
+
+    #vortex is given by location_dependent_delay which is the angle/(2*pi) in the x-y plane of the bond location around the vortex
+    if vortex_location == 'bond':
+        vortex_center = tuple(np.array(hexagonal_lattice_site_to_x_y((num_sites_x // 2, num_sites_y // 2, 0))) + np.array(
+            (0.5, 0)))  # on bond
+    if vortex_location == 'site':
+        vortex_center = tuple(np.array(hexagonal_lattice_site_to_x_y((num_sites_x // 2, num_sites_y // 2, 0))) + np.array(
+            (1, 0)))  # on site
+    if vortex_location == 'plaquette':
+        vortex_center = tuple(np.array(hexagonal_lattice_site_to_x_y((num_sites_x // 2, num_sites_y // 2, 0))) + np.array(
+            (2, 0)))  # on plaquette
+    if vortex_location is None:
+        location_dependent_delay = None
+    else:
+        location_dependent_delay = lambda x, y: np.arctan2(y - vortex_center[1], x - vortex_center[0]) / (-2*np.pi)
+
+    add_J_term(J_x_strength, 'x', hamiltonian, site_offset_x, 0, 1, lattice_shape, t0=0, location_dependent_delay=location_dependent_delay)
+    add_J_term(J_y_strength, 'y', hamiltonian, site_offset_y, 1, 0, lattice_shape, t0=1/3, location_dependent_delay=location_dependent_delay)
+    add_J_term(J_z_strength, 'z', hamiltonian, site_offset_z, 1, 0, lattice_shape, t0=2/3, location_dependent_delay=location_dependent_delay)
 
     return hamiltonian
 
 
-def add_J_term(J, alpha, hamiltonian, site_offset_x, sublattice1, sublattice2, lattice_shape, t0):
-    for site1 in np.ndindex(tuple(lattice_shape[:-1] - np.array(site_offset_x))):
-        site2 = tuple(np.array(site1) + np.array(site_offset_x))
-        hamiltonian.add_term(name=f'J{alpha}_{site1}', strength=J, sublattice1=sublattice1, sublattice2=sublattice2, site1=site1, site2=site2,
-                             time_dependence=lambda t: get_J(t-t0))
+def add_J_term(J, alpha, hamiltonian, site_offset, sublattice1, sublattice2, lattice_shape, t0, location_dependent_delay=None):
+    for site1 in np.ndindex(tuple(lattice_shape[:-1] - np.array(site_offset))):
+        site2 = tuple(np.array(site1) + np.array(site_offset))
+        J_on_site = J[site1] if isinstance(J, np.ndarray) else J
+        if location_dependent_delay is not None:
+            bond_center = get_x_y_of_bond_center_from_site1_site2((*site1, sublattice1), (*site2, sublattice2))
+            bond_delay = location_dependent_delay(*bond_center)
+        else:
+            bond_delay = 0
+        hamiltonian.add_term(name=f'J{alpha}_{site1}', strength=J_on_site, sublattice1=sublattice1, sublattice2=sublattice2, site1=site1, site2=site2,
+                             time_dependence=lambda t: get_J(t-t0-bond_delay))
 
 
 def hexagonal_lattice_site_to_x_y(site):
@@ -46,12 +66,19 @@ def hexagonal_lattice_site_to_x_y(site):
     return x, y
 
 
-def draw_state(state, system_shape, hamiltonian:MajoranaFreeFermionHamiltonian = None, circle_radius=1/3):
+def get_x_y_of_bond_center_from_site1_site2(site1, site2):
+    x1, y1 = hexagonal_lattice_site_to_x_y(site1)
+    x2, y2 = hexagonal_lattice_site_to_x_y(site2)
+    x, y = (x1+x2)/2, (y1+y2)/2
+    return x, y
+
+def draw_state(state, system_shape, hamiltonian:MajoranaFreeFermionHamiltonian = None, circle_radius=1/3, ax=None):
     # draw all sites as circles with size according to the absolute value of the state and color according to the phase
     # this part draws the hamiltonian terms as lines between the sites with color according to the term name being x, y or z
     # the lines are drawn at the center of the site but are not contained in a circle of radius circle_radius which is also drawn
     arrow_to_circle_scale = 0.9
-    fig, ax = plt.subplots()
+    if ax is None:
+        fig, ax = plt.subplots()
     xyz_to_color = {'x': 'b', 'y': 'g', 'z': 'r'}
     if hamiltonian is not None:
         for name, term in hamiltonian.terms.items():
@@ -64,7 +91,7 @@ def draw_state(state, system_shape, hamiltonian:MajoranaFreeFermionHamiltonian =
             x1_at_circle_edge, y1_at_circle_edge = x1 + circle_radius*(x2-x1), y1 + circle_radius*(y2-y1)
             x2_at_circle_edge, y2_at_circle_edge = x2 - circle_radius*(x2-x1), y2 - circle_radius*(y2-y1)
             color = xyz_to_color[name[1]]
-            plt.plot([x1_at_circle_edge, x2_at_circle_edge], [y1_at_circle_edge, y2_at_circle_edge], color=color, linewidth=2)
+            ax.plot([x1_at_circle_edge, x2_at_circle_edge], [y1_at_circle_edge, y2_at_circle_edge], color=color, linewidth=2)
     # this part draws the state by drawing arrows pointing according to the phase and with size according to the absolute value
     # the arrows are drawn at the center of the site and are contained in a circle of radius 1/2 which is also drawn
     lattice_shape = system_shape[:len(system_shape) // 2]
@@ -75,30 +102,54 @@ def draw_state(state, system_shape, hamiltonian:MajoranaFreeFermionHamiltonian =
         site_phase = phase[site]
         circle = plt.Circle((x, y), circle_radius, color='k', fill=False)
         ax.add_patch(circle)
-        strength = np.array(np.abs(state[site])/np.max(np.abs(state))).reshape(1,1)
+        strength = np.array(np.abs(state[site])**2/np.max(np.abs(state))**2).reshape(1,1)
         ax.quiver(x, y, arrow_to_circle_scale*2*circle_radius*np.cos(site_phase), arrow_to_circle_scale*2*circle_radius*np.sin(site_phase),
-                  # headlength=2*circle_radius,
-                  # headaxislength=2*circle_radius,
-                  # width=0.01,
-                  # headwidth=3*circle_radius,
                   scale=1,
                   units='xy', pivot='middle', alpha=strength)
-        plt.axis('equal')
+        ax.axis('equal')
 
 
 if __name__ == "__main__":
     integration_params = dict(name='vode', nsteps=2000, rtol=1e-10, atol=1e-14)
     J = 3*np.pi/4
-    hamiltonian = get_floquet_KSL_model(5,5, J=J)
-    unitary = hamiltonian.full_cycle_unitary_faster(integration_params, 0, 1)
-    plt.imshow(np.real(unitary))
-    plt.colorbar()
+
+    for vortex_location in ['plaquette']:
+        hamiltonian = get_floquet_KSL_model(2,2, J=J, vortex_location=vortex_location)
+        unitary = hamiltonian.full_cycle_unitary_faster(integration_params, 0, 1)
+        # draw the real and imaginary parts of the unitary as subplots with colorbars for each
+        fig, ax = plt.subplots(1, 2)
+        ax[0].imshow(np.real(unitary))
+        ax[0].set_title('real part')
+        ax[1].imshow(np.imag(unitary))
+        ax[1].set_title('imaginary part')
+        phases, states = eig(unitary)
+        energies = np.angle(phases)
+        # sort the energies and phases and states according to the energies
+        sort_indices = np.argsort(energies)
+        energies = energies[sort_indices]
+        phases = phases[sort_indices]
+        states = states[:, sort_indices]
+
+        # plot the energies
+        plt.figure(100)
+        plt.plot(energies/np.pi, '.')
+
     plt.show()
-    phases, states = eig(unitary)
-    energies = np.angle(phases)
-    # plt.figure()
-    # plt.plot(energies, 'o')
-    # plt.show()
+
+    # acting with the unitary on an initial state with a localized single fermion excitation
+    initial_state = np.zeros_like(states[:, 0])
+    initial_site = (0,0, 0)
+    initial_site_index = np.ravel_multi_index(initial_site, hamiltonian.system_shape[:len(hamiltonian.system_shape)//2])
+    initial_state[initial_site_index] = 1
+    final_state = unitary @ initial_state
+    # draw the initial and final states in subplots
+    _, ax = plt.subplots(1, 2)
+    draw_state(initial_state, hamiltonian.system_shape, hamiltonian, ax=ax[0])
+    draw_state(final_state, hamiltonian.system_shape, hamiltonian, ax=ax[1])
+    ax[0].set_title('initial state')
+    ax[1].set_title('final state')
+
+
     for state, energy in zip(states.T, energies):
         draw_state(state, hamiltonian.system_shape, hamiltonian)
         plt.title(f'energy/$\pi$ = {energy/np.pi}')
