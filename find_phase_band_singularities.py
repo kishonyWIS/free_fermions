02f180fe1fa00ebab3acdd8cd2547e78ef6ea329
giladkishony <giladkishony@gmail.com>
2024-07-30 23:39:30 +0300

    change kx,and ky in the numerical part of find_phase_band_singularities.py and phase_bands_invariants_at_time_slices.py

--- find_phase_band_singularities.py
+++ find_phase_band_singularities.py
@@ -8,12 +8,12 @@ import matplotlib
 from scipy.interpolate import LinearNDInterpolator
 
 
-def plot_singularities_3d(ky, res_grid, res_energy=0.1, ax=None, pulse_length = 1/3, plot=False, plot_numerical_singularities=False):
-    kx_list = np.linspace(0, np.pi, 101)
+def plot_singularities_3d(kx, res_grid, res_energy=0.1, ax=None, pulse_length = 1/3, plot=False, plot_numerical_singularities=False):
+    ky_list = np.linspace(0, np.pi, 101)
     theta_list = np.linspace(0, 2 * np.pi, 101)
-    KX, THETA = np.meshgrid(kx_list, theta_list, indexing='ij')
+    KY, THETA = np.meshgrid(ky_list, theta_list, indexing='ij')
 
-    if ky == 0.:
+    if kx == 0.:
         point_singularities_0 = [[np.pi, 0, 2/3]]
         point_singularities_pi = []
         line_singularities_0 = [[[np.pi, np.pi], [2*np.pi/3, 4*np.pi/3], [0, 2 / 3]],
@@ -23,15 +23,15 @@ def plot_singularities_3d(ky, res_grid, res_energy=0.1, ax=None, pulse_length = 
                                  [[0, 0],[0, 2*np.pi],[2/3, 2/3]]]
         plane_singularities_0 = []
         plane_singularities_pi = []
-    elif ky == np.pi:
+    elif kx == np.pi:
         point_singularities_0 = []
         point_singularities_pi = [[0,0,2/3], [0,2*np.pi,2/3], [np.pi,4/3*np.pi,2/3]]
         line_singularities_0 = [[[np.pi, np.pi], [0, 2 * np.pi / 3], [2/3, 2 / 3]],
                                 [[0, 0], [2 * np.pi / 3, 4 * np.pi / 3], [2/3, 2 / 3]],
                                 [[0, 0], [2 * np.pi / 3, 4 * np.pi / 3], [0, 2 / 3]],
                                 [[np.pi, np.pi], [4 * np.pi / 3, 2 * np.pi], [0, 2 / 3]]]
-        for kx in np.linspace(0,np.pi,100):
-            line_singularities_0.append([[kx,kx],[0,2/3*np.pi],[0,2/3]])
+        for ky in np.linspace(0,np.pi,100):
+            line_singularities_0.append([[ky,ky],[0,2/3*np.pi],[0,2/3]])
         line_singularities_pi = []
         plane_singularities_0 = [[[0,0,0],
                                  [np.pi,0,0],
@@ -40,20 +40,20 @@ def plot_singularities_3d(ky, res_grid, res_energy=0.1, ax=None, pulse_length = 
         plane_singularities_pi = []
 
 
-    kx_list = np.linspace(0, np.pi, res_grid//2)
+    ky_list = np.linspace(0, np.pi, res_grid//2)
     theta_list = np.linspace(0, 2 * np.pi, res_grid)
     times = np.linspace(0, 1, res_grid)
 
-    u = np.zeros((len(kx_list), len(theta_list), len(times), 2, 2), dtype=np.complex128)
-    angles = np.zeros((len(kx_list), len(theta_list), len(times), 2))
-    states = np.zeros((len(kx_list), len(theta_list), len(times), 2, 2), dtype=np.complex128)
+    u = np.zeros((len(ky_list), len(theta_list), len(times), 2, 2), dtype=np.complex128)
+    angles = np.zeros((len(ky_list), len(theta_list), len(times), 2))
+    states = np.zeros((len(ky_list), len(theta_list), len(times), 2, 2), dtype=np.complex128)
 
 
-    for i_kx, kx in enumerate(kx_list):
-        print(i_kx)
+    for i_ky, ky in enumerate(ky_list):
+        print(i_ky)
         for i_theta, theta in enumerate(theta_list):
             for i_t, time in enumerate(times):
-                angles[i_kx, i_theta, i_t], states[i_kx, i_theta, i_t, :] = diagonalize_unitary_at_k_theta_time(kx, ky, theta, time, pulse_length=pulse_length)
+                angles[i_ky, i_theta, i_t], states[i_ky, i_theta, i_t, :] = diagonalize_unitary_at_k_theta_time(kx, ky, theta, time, pulse_length=pulse_length)
 
     top_band_phases = np.abs(angles.max(axis=-1))
     topological_singularities_pi = top_band_phases > np.pi - res_energy
@@ -68,15 +68,15 @@ def plot_singularities_3d(ky, res_grid, res_energy=0.1, ax=None, pulse_length = 
 
     size = 21
     pos = np.where(topological_singularities_0)
-    zorder_0 = 1 if ky == np.pi else 30
+    zorder_0 = 1 if kx == np.pi else 30
     if plot_numerical_singularities:
-        s = ax.scatter(kx_list[pos[0]], theta_list[pos[1]], times[pos[2]], c='r', s=20, alpha=1, zorder=zorder_0)
+        s = ax.scatter(ky_list[pos[0]], theta_list[pos[1]], times[pos[2]], c='r', s=20, alpha=1, zorder=zorder_0)
     if plot:
         for point in point_singularities_0:
             # ax.scatter(point[0], point[1], point[2], c='r', s=size, alpha=1, zorder=zorder_0)
             ax.plot([point[0]]*2, [point[1]]*2, [point[2],point[2]-0.002], c='r', alpha=1, linewidth=5,zorder=zorder_0)
         for line in line_singularities_0:
-            ax.plot(line[0], line[1], line[2], c='r', alpha=1, linewidth=5,zorder=zorder_0)
+            ax.plot(line[0], line[1], line[2], c='r', alpha=1, linewidth=5, zorder=zorder_0)
     # for plane in plane_singularities_0:
     #     interp = LinearNDInterpolator(np.array(plane)[:, :-1], np.array(plane)[:, -1])
     #     TIME = interp(KX, THETA)
@@ -86,7 +86,7 @@ def plot_singularities_3d(ky, res_grid, res_energy=0.1, ax=None, pulse_length = 
     pos = np.where(topological_singularities_pi)
 
     if plot_numerical_singularities:
-        s = ax.scatter(kx_list[pos[0]], theta_list[pos[1]], times[pos[2]], c='b', s=20, alpha=1, zorder=30)
+        s = ax.scatter(ky_list[pos[0]], theta_list[pos[1]], times[pos[2]], c='b', s=20, alpha=1, zorder=30)
     # ax.plot([0, np.pi], [2 / 3 * np.pi, 2 / 3 * np.pi], [2 / 3, 2 / 3], c='b', alpha=1, linewidth=5,zorder=30)
     if plot:
         for point in point_singularities_pi:
--- phase_bands_invariants_at_time_slices.py
+++ phase_bands_invariants_at_time_slices.py
@@ -56,19 +56,19 @@ plt.rcParams.update({
     '''
 })
 
-kx_list = np.linspace(0, np.pi, 101)
-ky = np.pi#np.pi#0.#
+ky_list = np.linspace(0, np.pi, 101)
+kx = 0.#np.pi#0.#
 pulse_length = 1/3
 theta_list = np.linspace(0, 2*np.pi, 101)
-KX, THETA = np.meshgrid(kx_list, theta_list, indexing='ij')
+KY, THETA = np.meshgrid(ky_list, theta_list, indexing='ij')
 
-u = np.zeros((len(kx_list), len(theta_list), 2, 2), dtype=np.complex128)
-angles = np.zeros((len(kx_list), len(theta_list), 2))
-states = np.zeros((len(kx_list), len(theta_list), 2, 2), dtype=np.complex128)
+u = np.zeros((len(ky_list), len(theta_list), 2, 2), dtype=np.complex128)
+angles = np.zeros((len(ky_list), len(theta_list), 2))
+states = np.zeros((len(ky_list), len(theta_list), 2, 2), dtype=np.complex128)
 
 
 
-if ky == 0.:
+if kx == 0.:
     anchors = [np.array([0,0,0.5]),
                np.array([0,2*np.pi,0.5]),
                np.array([np.pi,0,5/6]),
@@ -78,7 +78,7 @@ if ky == 0.:
 
 # between the zero and pi singularities for ky=np.pi
 
-if ky == np.pi:
+if kx == np.pi:
     anchors = [np.array([0,2*np.pi,0.5]),
                np.array([0,0,0.5]),
                np.array([np.pi,2*np.pi*2/3,0.5]),
@@ -89,22 +89,22 @@ if ky == np.pi:
                np.array([np.pi,0,5/6])]
 
 interp = LinearNDInterpolator(np.array(anchors)[:,:-1], np.array(anchors)[:,-1])
-TIME = interp(KX, THETA)
+TIME = interp(KY, THETA)
 
-for i_kx, kx in enumerate(kx_list):
-    print(i_kx)
+for i_ky, ky in enumerate(ky_list):
+    print(i_ky)
     for i_theta, theta in enumerate(theta_list):
-        point = np.array([kx, theta])
-        time = TIME[i_kx, i_theta]
+        point = np.array([ky, theta])
+        time = TIME[i_ky, i_theta]
         # ang, stat = diagonalize_unitary_at_k_theta_time(0.4, 0.7, 0, 1, pulse_length=pulse_length)
-        angles[i_kx, i_theta, :], states[i_kx, i_theta, :, :] = diagonalize_unitary_at_k_theta_time(kx, ky, theta, time, pulse_length = pulse_length)
+        angles[i_ky, i_theta, :], states[i_ky, i_theta, :, :] = diagonalize_unitary_at_k_theta_time(kx, ky, theta, time, pulse_length = pulse_length)
 
 
 top_band_phases = np.abs(angles.max(axis=-1))
 top_band_states = states[:, :, :, 0]
 
-plt.pcolor(KX,THETA,top_band_phases)
-plt.xlabel('kx')
+plt.pcolor(KY,THETA,top_band_phases)
+plt.xlabel('ky')
 plt.ylabel('theta')
 plt.colorbar()
 
@@ -123,17 +123,17 @@ ax.set_yticks([0, 2 * np.pi / 3, 2 * np.pi * 2 / 3, 2 * np.pi])
 ax.set_yticklabels([0, '$\\frac{2\pi}{3}$', '$\\frac{4\pi}{3}$', '$2\pi$'])
 ax.set_zticks([0, 1 / 3, 2 / 3, 1])
 ax.set_zticklabels([0, '$T/3$', '$2T/3$', '$T$'])
-edit_graph('$k_x$', '$\\theta$', '$t$', ax)
+edit_graph('$k_y$', '$\\theta$', '$t$', ax)
 
 mode = 'numerical' #'manual'
 if mode == 'numerical':
     point_singularities_0, point_singularities_pi, line_singularities_0, line_singularities_pi =\
-        plot_singularities_3d(ky, 22, 0.05, ax, pulse_length=pulse_length, plot=False, plot_numerical_singularities=True)
+        plot_singularities_3d(kx, 22, 0.05, ax, pulse_length=pulse_length, plot=False, plot_numerical_singularities=True)
 if mode == 'manual':
     point_singularities_0, point_singularities_pi, line_singularities_0, line_singularities_pi =\
-        plot_singularities_3d(ky, 22, 0.05, ax, pulse_length=pulse_length, plot=False, plot_numerical_singularities=False)
+        plot_singularities_3d(kx, 22, 0.05, ax, pulse_length=pulse_length, plot=False, plot_numerical_singularities=False)
 
-    surf = ax.plot_surface(KX, THETA, TIME, cmap=matplotlib.cm.plasma.reversed(), alpha=0.7, linewidth=0,
+    surf = ax.plot_surface(KY, THETA, TIME, cmap=matplotlib.cm.plasma.reversed(), alpha=0.7, linewidth=0,
                            antialiased=True, zorder=3)
 
     anchors = np.array(anchors)
@@ -142,7 +142,7 @@ if mode == 'manual':
     for simplex in tri.simplices:
         ax.plot_trisurf(anchors[simplex, 0], anchors[simplex, 1], anchors[simplex, 2], color='none', edgecolor='k')
 
-    zorder_0 = 1 if ky == np.pi else 30
+    zorder_0 = 1 if kx == np.pi else 30
     for point in point_singularities_0:
         # ax.scatter(point[0], point[1], point[2], c='r', s=size, alpha=1, zorder=zorder_0)
         ax.plot([point[0]] * 2, [point[1]] * 2, [point[2], point[2] - 0.002], c='r', alpha=1, linewidth=5, zorder=zorder_0)
@@ -155,27 +155,9 @@ if mode == 'manual':
         ax.plot(line[0], line[1], line[2], c='b', alpha=1, linewidth=5, zorder=30)
 
 
-    plt.savefig(f'graphs/time_vortex/kx_theta_t_space_ky_{ky:.2f}.pdf')
+    plt.savefig(f'graphs/time_vortex/ky_theta_t_space_kx_{kx:.2f}.pdf')
 
 
-
-
-    # fig = go.Figure()
-    # fig.add_trace(go.Scatter(
-    #     x=[1, 2, 3, 4],
-    #     y=[1, 4, 9, 16],
-    #     name=r'$\alpha_{1c} = 352 \pm 11 \text{ km s}^{-1}$'
-    # ))
-    # fig.add_trace(go.Scatter(
-    #     x=[1, 2, 3, 4],
-    #     y=[0.5, 2, 4.5, 8],
-    #     name=r'$\beta_{1c} = 25 \pm 11 \text{ km s}^{-1}$'
-    # ))
-    # fig.update_layout(
-    #     xaxis_title=r'$\sqrt{(n_\text{c}(t|{T_\text{early}}))}$',
-    #     yaxis_title=r'$d, r \text{ (solar radius)}$'
-    # )
-    # fig.show()
 
 
 
@@ -184,7 +166,7 @@ if mode == 'manual':
     fig = go.Figure()
 
     fig.add_trace(go.Surface(
-        x=KX,
+        x=KY,
         y=THETA,
         z=TIME,
         colorscale='Plasma',
@@ -240,7 +222,7 @@ if mode == 'manual':
         ))
 
     fig.update_layout(scene=dict(
-        xaxis_title=dict(text=u'k\u2093', font=dict(size=50)),
+        xaxis_title=dict(text=u'kᵧ', font=dict(size=50)),
         yaxis_title=dict(text=r'θ', font=dict(size=50)),
         zaxis_title=dict(text=r't', font=dict(size=50)),
         camera=dict(
@@ -269,7 +251,7 @@ if mode == 'manual':
     ))
     fig.update_scenes(yaxis_autorange="reversed")
 
-    fig.write_html(f'graphs/time_vortex/kx_theta_t_space_ky_{ky:.2f}.html')
+    fig.write_html(f'graphs/time_vortex/ky_theta_t_space_kx_{kx:.2f}.html')
     fig.show()
 
 
